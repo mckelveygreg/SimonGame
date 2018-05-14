6913cb63363e09b36308829169d1ea28cd13cc2f
mckelveygreg <mckelveygreg@gmail.com>
2018-05-13 22:33:03 -0700

    Random color picker

--- script.js
+++ script.js
@@ -10,13 +10,33 @@ var sounds = {
 // var soundYellow = new Audio('sounds/simonSound3.mp3');
 // var soundRed = new Audio('sounds/simonSound4.mp3');
 
-var gameboard = document.querySelectorAll('.colors');
+var colors = document.querySelectorAll('.colors');
+var gameboard = {}; // do i need this?
 
-gameboard.forEach(function(color) {
+colors.forEach(function(color) {
     var id = color.id;
-    console.log(sounds[id]);
     color.addEventListener('click', function() {
         sounds[id].play();
     });
+    gameboard = {id: id}; // doesn't add each to gameboard
 });
 
+var randomColorPlayer = function() {
+    var randomColor = colors[Math.floor(Math.random()*colors.length)].id;
+    sounds[randomColor].play();
+    console.log(randomColor);
+}
+
+
+/*TODO
+-- Study TicTacToe state switching
+ -- AI
+    - Make random color player
+    - Record action in array
+    - Game state switch
+-- Player
+    - Vet each choice against answer array
+    - While in answer array
+    - If correct choice, move to next index
+    - 
+*/
